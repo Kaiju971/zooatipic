7ff5f5615a9bfce49fbd9367342838f8a26636dd
Kaiju971 <129183625+Kaiju971@users.noreply.github.com>
2024-09-17 09:53:20 +0200

    loi

---
+++ packages/client/src/pages/loi/loi.styled.tsx
@@ -0,0 +1,11 @@
+import { styled } from "@mui/material/styles";
+
+export const MainContainer = styled("div")`
+  color: darkred;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.3);
+  padding: 0;
+  justify-content: center;
+  text-align: center;
+`;
